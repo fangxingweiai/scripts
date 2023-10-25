e063d2797634d20239dce7a53cb33e944e3a0ef1
Gan Kang <gankangs@163.com>
2023-10-26 01:17:32 +0800

    优化钢镚阅读

--- sc_czgm_pro.py
+++ sc_czgm_pro.py
@@ -16,7 +16,7 @@
 # 软件版本
 """
 cron: 3 * * * *
-new Env('钢镚(充值购买)阅读');
+new Env('钢镚阅读');
 """
 name = "充值购买(钢镚)"
 linxi_token = "gbtoken"
@@ -42,8 +42,6 @@ Limit = 2
 
 # 充值购买(钢镚)域名(无法使用时请更换)
 domain = 'http://2496831.marskkqh7ij0j.jpsl.u1jcnc75wwbyk.cloud'
-# 保持连接,重复利用
-ss = requests.session()
 
 # 检测文章列表(如有未收录可自行添加)
 check_list = [
@@ -63,137 +61,137 @@ def get_sign():
     return data
 
 
-# 获取个人信息模块
-def user_info(i, ck):
-    # 请求头
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64",
-        "Cookie": f"gfsessionid={ck['ck']};"
-    }
-    try:
-        result = ss.get(domain + "/share", headers=headers, data=get_sign()).json()
-        share_link = result["data"]["share_link"][0]
-        userid = share_link.split("=")[1].split("&")[0]
-    except:
-        print(f"账号【{i + 1}】请检查你的CK({ck['ck']})是否正确!")
-        return False
-    result = ss.get(domain + "/read/info", headers=headers, data=get_sign()).json()
-    if result["code"] == 0:
-        remain = result["data"]["remain"]
-        read = result["data"]["read"]
-        print(f"账号【{i + 1}】UID:{userid} 余额:{remain} 今日:{read}篇 推广:{share_link}")
-    else:
-        print(f'账号【{i + 1}】用户信息获取失败:{result["message"]}')
+class Task:
+    def __init__(self, index, ck):
+        # 保持连接,重复利用
+        self.ss = requests.session()
 
+        self.index = index
+        self.ck = ck
 
-# 阅读文章模块
-def do_read(args):
-    i, ck = args
-    # 请求头
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64",
-        "Cookie": f"gfsessionid={ck['ck']};"
-    }
-    while True:
-        response = ss.get(domain + "/read/task", headers=headers, data=get_sign())
+    # 获取个人信息模块
+    def user_info(self):
+        # 请求头
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64",
+            "Cookie": f"gfsessionid={self.ck['ck']};"
+        }
+        try:
+            result = self.ss.get(f"{domain}/share", headers=headers, data=get_sign()).json()
+            share_link = result["data"]["share_link"][0]
+            userid = share_link.split("=")[1].split("&")[0]
+        except:
+            print(f"账号【{self.index}】请检查你的CK({self.ck['ck']})是否正确!")
+            return
+        result = self.ss.get(f"{domain}/read/info", headers=headers, data=get_sign()).json()
+        if result["code"] == 0:
+            remain = result["data"]["remain"]
+            read = result["data"]["read"]
+            print(f"账号【{self.index}】UID:{userid} 余额:{remain} 今日:{read}篇 推广:{share_link}")
+        else:
+            print(f'账号【{self.index}】用户信息获取失败:{result["message"]}')
+
+    # 阅读文章模块
+    def do_read(self):
+        # 请求头
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64",
+            "Cookie": f"gfsessionid={self.ck['ck']};"
+        }
+        while True:
+            response = self.ss.get(f"{domain}/read/task", headers=headers, data=get_sign())
+            try:
+                response = response.json()
+            except json.decoder.JSONDecodeError:
+                print(f"账号【{self.index}】请检查你的CK({self.ck['ck']})是否正确!")
+                return
+            if response["code"] == 1:
+                if "秒" in response['message']:
+                    print(f"账号【{self.index}】即将开始:{response['message']}")
+                    time.sleep(5)
+                elif response['message'] == "记录失效":
+                    print(f"账号【{self.index}】阅读异常,重新获取:{response['message']}")
+                else:
+                    print(f"账号【{self.index}】{response['message']}")
+                    break
+            else:
+                try:
+                    s = random.randint(10, 12)
+                    # 检测是否是检测文章
+                    biz = re.findall("biz=(.*?)&", response["data"]["link"])[0]
+                    print(f"账号【{self.index}】获取文章成功-{biz}-模拟{s}秒")
+                    if biz in check_list:
+                        print(f"账号【{self.index}】阅读检测文章-准备推送微信验证!")
+                        # 过检测
+                        self.check_status(response["data"]["link"])
+
+                        response = self.ss.post(f"{domain}/read/finish", headers=headers, data=get_sign()).json()
+                        print(
+                            f'账号【{self.index}】阅读文章成功-获得钢镚[{response["data"]["gain"]}]-已读{response["data"]["read"]}篇')
+
+                    else:
+                        time.sleep(s)
+                        response = self.ss.post(f"{domain}/read/finish", headers=headers, data=get_sign()).json()
+                        # print(response)
+                        if response["code"] == 0:
+                            if response["data"]["check"] is False:
+                                print(
+                                    f'账号【{self.index}】阅读文章成功-获得钢镚[{response["data"]["gain"]}]-已读{response["data"]["read"]}篇')
+                            else:
+                                print(f"账号【{self.index}】获取到未收录检测: {biz} 将自动停止脚本")
+                                break
+                        else:
+                            if response['message'] == "记录无效":
+                                print(f"账号【{self.index}】记录无效,重新阅读")
+                            else:
+                                print(f"账号【{self.index}】{response}")
+                                break
+                except KeyError:
+                    if response['code'] == 801:
+                        print(f"账号【{self.index}】今日任务已完成: {response['message']}")
+                        break
+                    else:
+                        print(f"账号【{self.index}】获取文章失败,错误未知{response}")
+                        break
+
+    # 提现模块
+    def get_money(self):
+        # 请求头
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64",
+            "Cookie": f"gfsessionid={self.ck['ck']};"
+        }
+        response = self.ss.get(f"{domain}/read/info", headers=headers, data=get_sign())
         try:
             response = response.json()
         except json.decoder.JSONDecodeError:
-            print(f"账号【{i + 1}】请检查你的CK({ck['ck']})是否正确!")
-            return False
-        if response["code"] == 1:
-            if "秒" in response['message']:
-                print(f"账号【{i + 1}】即将开始:{response['message']}")
-                time.sleep(5)
-            elif response['message'] == "记录失效":
-                print(f"账号【{i + 1}】阅读异常,重新获取:{response['message']}")
-            else:
-                print(f"账号【{i + 1}】{response['message']}")
-                break
-        else:
-            try:
-                s = random.randint(10, 12)
-                # 检测是否是检测文章
-                biz = re.findall("biz=(.*?)&", response["data"]["link"])[0]
-                print(f"账号【{i + 1}】获取文章成功-{biz}-模拟{s}秒")
-                if biz in check_list:
-                    print(f"账号【{i + 1}】阅读检测文章-已推送微信,请40s内完成验证!")
-                    # 过检测
-                    check = check_status(ck['ts'], response["data"]["link"], i)
-                    if check == True:
-                        print(f"账号【{i + 1}】检测文章-过检测成功啦!")
-                        response = ss.post(domain + "/read/finish", headers=headers, data=get_sign()).json()
-                        print(
-                            f'账号【{i + 1}】阅读文章成功-获得钢镚[{response["data"]["gain"]}]-已读{response["data"]["read"]}篇')
-                    else:
-                        print(f"账号【{i + 1}】检测文章-过检测失败啦!")
-                        break
-                else:
-                    time.sleep(s)
-                    response = ss.post(domain + "/read/finish", headers=headers, data=get_sign()).json()
-                    # print(response)
-                    if response["code"] == 0:
-                        if response["data"]["check"] is False:
-                            print(
-                                f'账号【{i + 1}】阅读文章成功-获得钢镚[{response["data"]["gain"]}]-已读{response["data"]["read"]}篇')
-                        else:
-                            print(f"账号【{i + 1}】获取到未收录检测: {biz} 将自动停止脚本")
-                            break
-                    else:
-                        if response['message'] == "记录无效":
-                            print(f"账号【{i + 1}】记录无效,重新阅读")
-                        else:
-                            print(f"账号【{i + 1}】{response}")
-                            break
-            except KeyError:
-                if response['code'] == 801:
-                    print(f"账号【{i + 1}】今日任务已完成: {response['message']}")
-                    break
-                else:
-                    print(f"账号【{i + 1}】获取文章失败,错误未知{response}")
-                    break
-
-
-# 提现模块
-def get_money(i, ck):
-    # 请求头
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 12; Redmi K30 Pro Build/SKQ1.220303.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5279 MMWEBSDK/20230805 MMWEBID/3850 MicroMessenger/8.0.41.2441(0x28002951) WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64",
-        "Cookie": f"gfsessionid={ck['ck']};"
-    }
-    response = ss.get(domain + "/read/info", headers=headers, data=get_sign())
-    try:
-        response = response.json()
-    except json.decoder.JSONDecodeError:
-        print(f"账号【{i + 1}】请检查你的CK({ck['ck']})是否正确!")
-        return False
-    if response["code"] == 0:
-        remain = response["data"]["remain"]
-    else:
-        print(f'账号【{i + 1}】获取用户信息失败: {response["message"]}')
-    if remain >= Limit * 10000:
-        response = ss.get(domain + "/withdraw/wechat", headers=headers, data=get_sign()).json()
+            print(f"账号【{self.index}】请检查你的CK({self.ck['ck']})是否正确!")
+            return
         if response["code"] == 0:
-            print(f'账号【{i + 1}】开始提现:{response["message"]}')
-        elif response["code"] == 1:
-            print(f'账号【{i + 1}】开始提现:{response["message"]}')
+            remain = response["data"]["remain"]
+            if remain >= Limit * 10000:
+                response = self.ss.get(f"{domain}/withdraw/wechat", headers=headers, data=get_sign()).json()
+                if response["code"] == 0:
+                    print(f'账号【{self.index}】开始提现:{response["message"]}')
+                elif response["code"] == 1:
+                    print(f'账号【{self.index}】开始提现:{response["message"]}')
+                else:
+                    print(f'账号【{self.index}】未知错误:{response}')
+            else:
+                print(f'账号【{self.index}】当前余额为{remain} 未到达{Limit}元提现限制!')
         else:
-            print(f'账号【{i + 1}】未知错误:{response}')
-    else:
-        print(f'账号【{i + 1}】当前余额为{remain} 未到达{Limit}元提现限制!')
+            print(f'账号【{self.index}】获取用户信息失败: {response["message"]}')
 
+    # 微信推送模块
+    def check_status(self, link):
+        with R:
+            time.sleep(5)
 
-# 微信推送模块
-def check_status(key, link, index):
-    with R:
-        time.sleep(5)
-
-    result = ss.get(
-        f'https://wxpusher.zjiecode.com/demo/send/custom/{key}?content=检测文章-{name}%0A请在{tsleep}秒内完成验证!%0A%3Cbody+onload%3D%22window.location.href%3D%27{quote(link)}%27%22%3E').json()
-    print(f"账号【{str(index + 1)}】微信消息推送: {result['msg']},等待{tsleep}s完成验证!")
-    # print(f"手动微信阅读链接: {link}")
-    time.sleep(tsleep)
-    return True
+        result = self.ss.get(
+            f'https://wxpusher.zjiecode.com/demo/send/custom/{self.ck["ts"]}?content=检测文章-{name}%0A请在{tsleep}秒内完成验证!%0A%3Cbody+onload%3D%22window.location.href%3D%27{quote(link)}%27%22%3E').json()
+        print(f"账号【{self.index}】微信消息推送: {result['msg']},等待{tsleep}s完成验证!")
+        # print(f"手动微信阅读链接: {link}")
+        time.sleep(tsleep)
 
 
 def main():
@@ -203,17 +201,23 @@ def main():
 
     ck_token = [json.loads(line) for line in os.getenv(linxi_token).splitlines()]
 
+    tasks = []
+    for idx, ck in enumerate(ck_token, start=1):
+        task = Task(idx, ck)
+        tasks.append(task)
+
     print("==================开始阅读文章=================")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=len(ck_token)) as executor:
-        executor.map(do_read, list(enumerate(ck_token)))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(tasks)) as executor:
+        for task in tasks:
+            executor.submit(task.do_read)
 
     print("==================获取账号信息=================")
-    for i, ck in list(enumerate(ck_token)):
-        user_info(i, ck)
+    for task in tasks:
+        task.user_info()
 
     print("==================开始账号提现=================")
-    for i, ck in list(enumerate(ck_token)):
-        get_money(i, ck)
+    for task in tasks:
+        task.get_money()
 
 
 if __name__ == "__main__":
